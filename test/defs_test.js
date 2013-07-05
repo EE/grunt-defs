be36117cbc7100cb19d0d3185d88f0d39f6cfdc9
Michał Gołębiowski <m.goleb@gmail.com>
2013-07-05 13:27:27 +0200

    update dependencies, fix tests

--- test/defs_test.js
+++ test/defs_test.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const grunt = require('grunt');
+const defs = require('defs');
+const fs = require('fs');
 
 /*
  ======== A Handy Little Nodeunit Reference ========
@@ -24,15 +26,28 @@ const grunt = require('grunt');
 
 exports.defs = {
     setUp: function (done) {
-
+        this.options = {
+            outputFileSuffix: '.defs',
+            defsOptions: {
+                disallowVars: true,
+                disallowDuplicated: true,
+                disallowUnknownReferences: false,
+            },
+        };
         done();
     },
     simple_example: function (test) {
-        test.expect(1);
+        test.expect(2);
 
-        const actual = grunt.file.read('test/tmp/simple-example.js');
-        const expected = grunt.file.read('test/expected/simple-example.js');
-        test.equal(actual, expected, 'should transform consts & lets to vars.');
+        const defsOutput = defs(grunt.file.read('test/fixtures/simple_example.js'), this.options);
+
+        test.equal(defsOutput.exitcode, 0, 'should exit with code 0.');
+        if (defsOutput.exitcode !== 0) {
+            return;
+        }
+
+        const expected = grunt.file.read('test/expected/simple_example.js');
+        test.equal(defsOutput.src, expected, 'should transform consts & lets to vars.');
 
         test.done();
     },
